e6d51bbbbdc8b76df13b670961536efe1e8a000d
Olivia Gometz <oliviagometz@gmail.com>
2018-04-16 21:30:14 +0200

    Mise à jour modèle BDD + clean view Home

--- cookblog.sql
+++ cookblog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  Dim 15 avr. 2018 à 20:28
+-- Généré le :  lun. 16 avr. 2018 à 21:29
 -- Version du serveur :  5.7.17
 -- Version de PHP :  7.1.3
 
@@ -25,6 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `etapes`
+--
+
+CREATE TABLE `etapes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `recette` mediumint(8) UNSIGNED NOT NULL,
+  `numero` tinyint(3) UNSIGNED NOT NULL,
+  `instruction` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ingredients`
+--
+
+CREATE TABLE `ingredients` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `recettes`
 --
 
@@ -32,9 +56,6 @@ CREATE TABLE `recettes` (
   `id` mediumint(8) UNSIGNED NOT NULL,
   `nom` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `ingredients` text NOT NULL,
-  `etapes` text NOT NULL,
-  `ustensiles` text NOT NULL,
   `preparation` time NOT NULL,
   `cuisson` time NOT NULL,
   `difficulte` tinyint(3) UNSIGNED NOT NULL,
@@ -49,12 +70,39 @@ CREATE TABLE `recettes` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `recettesingredients`
+--
+
+CREATE TABLE `recettesingredients` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `recette` mediumint(8) UNSIGNED NOT NULL,
+  `ingredient` int(10) UNSIGNED NOT NULL,
+  `unite` tinyint(3) UNSIGNED NOT NULL,
+  `quantite` smallint(5) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `recettesustensiles`
+--
+
+CREATE TABLE `recettesustensiles` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `recette` mediumint(8) UNSIGNED NOT NULL,
+  `ustensile` smallint(5) UNSIGNED NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `unites`
 --
 
 CREATE TABLE `unites` (
   `id` tinyint(3) UNSIGNED NOT NULL,
   `nom` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `quantifiable` tinyint(1) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -65,10 +113,21 @@ CREATE TABLE `unites` (
 --
 
 CREATE TABLE `users` (
-  `id` tinyint(4) NOT NULL,
+  `id` tinyint(4) UNSIGNED NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` char(32) NOT NULL,
   `pseudo` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ustensiles`
+--
+
+CREATE TABLE `ustensiles` (
+  `id` smallint(5) UNSIGNED NOT NULL,
+  `nom` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -76,9 +135,33 @@ CREATE TABLE `users` (
 --
 
 --
+-- Index pour la table `etapes`
+--
+ALTER TABLE `etapes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `ingredients`
+--
+ALTER TABLE `ingredients`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Index pour la table `recettes`
 --
 ALTER TABLE `recettes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `recettesingredients`
+--
+ALTER TABLE `recettesingredients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `recettesustensiles`
+--
+ALTER TABLE `recettesustensiles`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -94,14 +177,40 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `ustensiles`
+--
+ALTER TABLE `ustensiles`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
+--
+-- AUTO_INCREMENT pour la table `etapes`
+--
+ALTER TABLE `etapes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `ingredients`
+--
+ALTER TABLE `ingredients`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `recettes`
 --
 ALTER TABLE `recettes`
   MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `recettesingredients`
+--
+ALTER TABLE `recettesingredients`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `recettesustensiles`
+--
+ALTER TABLE `recettesustensiles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT pour la table `unites`
 --
@@ -111,7 +220,12 @@ ALTER TABLE `unites`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `id` tinyint(4) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `ustensiles`
+--
+ALTER TABLE `ustensiles`
+  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- site/application/config/database.php
+++ site/application/config/database.php
@@ -74,12 +74,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'mysql:host=localhost; dbname=cookblog; charset=utf8',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
+	'database' => 'cookblog',
+	'dbdriver' => 'pdo',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
