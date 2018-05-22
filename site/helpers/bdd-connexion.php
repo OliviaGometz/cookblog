826addfd91714d25a5acc0ef8e6d3e27608fb210
Olivia Gometz <oliviagometz@gmail.com>
2018-05-22 23:09:57 +0200

    feat: force encodage utf8 sur pdo bdd

--- site/helpers/bdd-connexion.php
+++ site/helpers/bdd-connexion.php
@@ -1,7 +1,8 @@
 <?php
 
 try {
-	$bdd = new PDO('mysql:host=localhost;dbname=cookblog', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=cookblog', 'root', '', array('charset' => 'utf8'));
+	$bdd->query('SET CHARACTER SET utf8');
 }
 catch(Exception $e){
 	die('Error: '.$e->getMessage());
